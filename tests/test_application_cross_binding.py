e6ab32c81c0dd58e8620c31405a2a0d4364eba5d
SPRIME01 <SPRIME01@GMAIL.COM>
2026-09-26 11:45:59 -0400

    test(release): regenerate 0.18.1 cross-binding goldens

--- tests/test_application_cross_binding.py
+++ tests/test_application_cross_binding.py
@@ -25,7 +25,7 @@ import domainforge
 
 
 CONTRACT_GOLDEN_SHA256 = (
-    "sha256:78fa1c173ca7da383c5082b6bc2a442faf6280e24c2a6796bc99a34aa8a369cb"
+    "sha256:e2510cdb56fa43b684fd6aaeeb87749fb8dab413f5bff3854d122da53be94487"
 )
 
 
